6847183e0922d9dbb09c48642f4722b1d1f1c851
Kirill Fyodorov <kirill-fjodorov00000@yandex.ru>
2019-05-12 23:25:05 +0300

    Add PID regulator and new new verion main program

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ controller = TouchSensor(INPUT_1)
 m_c.position = 0
 m_a.position = 0
 ctPos = 0
+step = 90
 
+ki = 0.01
+kd = 0.01
+kp = 2
 
 
 def motorsReset():
@@ -36,24 +40,49 @@ def motorsControll(command):
         m_b.wait_while('running')
         if (m_b.is_holding):
             while(1):
-                m_a.run_timed(time_sp=100, speed_sp=100, stop_action='coast')
+                m_a.run_timed(time_sp=100, speed_sp=100, stop_action='hold')
                 m_a.wait_while('running' )
                 if (controller.is_pressed):
+                    m_a.stop(stop_action='coast')
                     break
             if not (m_a.is_running):
+                m_a.position = 0
                 print('Calibration was successful')
 
 def pointControll(fPos):
+    global ctPos
     key = int(fPos - ctPos)
-    speed = (1 if key > 0 else -1) * 62
+    sign = (1 if key > 0 else -1)
     key = abs(key)
+    ctPos = ctPos + fPos
     while(key):
-        m_a.run_timed(time_sp=1600, speed_sp=speed, stop_action='hold')
-        m_a.wait_while('running')
-        if not (m_a.is_running):
-            key = key - 1
-            time.sleep(1)
-
+        i = 0
+        fAng = step * sign + m_a.position
+        dPr = 0
+        while (1):
+            e = fAng - m_a.position
+            i = i + ki * e
+            d = kd * (e - dPr)
+            u = (kp * e + i + d)
+            if (u > 40):
+                u = 40
+            if (u < -40):
+                u = -40
+            if (u > 1 and u < 7):
+                u = 7
+            if (u > -7 and u < -1):
+                u = -7
+            m_a.run_direct(duty_cycle_sp=u)
+            dPr = e
+            time.sleep(0.01)
+            print(m_a.position)
+            if ((m_a.position - fAng < 3) and ((m_a.position - fAng > -3))):
+                m_a.stop(stop_action='hold')
+                break
+        print('finish')
+        key = key - 1
+        time.sleep(1)
+    print(ctPos)
 
 
 def getch():
@@ -74,34 +103,32 @@ def getch():
 motorsReset()
 log = 0
 motorsControll('calibration')
-i = 0
-while (i < 6):
-    pointControll(-1)
-    i+=1
 
-# while (1):
-#     # pressedKey = getch()
-#     pressedKey = input()
-#     if pressedKey == '1': pointControll(1)
-#     if pressedKey == '2': pointControll(2)
-#     if pressedKey == '3': pointControll(3)
-#     if pressedKey == '4': pointControll(4)
-#     if pressedKey == '5': pointControll(-1)
-#     if pressedKey == '6': pointControll(-2)
-#     if pressedKey == '7': pointControll(-3)
-#     if pressedKey == '8': pointControll(-4)
-#
-#
-#
-#     if pressedKey == 'w': motorsControll('up')
-#     if pressedKey == 's': motorsControll('down')
-#     if pressedKey == ' ':
-#         if (log == 0):
-#             motorsControll('take')
-#             log = 1
-#         else:
-#             motorsControll('drop')
-#             log = 0
-#     if pressedKey == 'q': break
-#
-# motorsReset()
+while (1):
+    # pressedKey = getch()
+    pressedKey = input()
+    if pressedKey == '1': pointControll(-1)
+    if pressedKey == '2': pointControll(-2)
+    if pressedKey == '3': pointControll(-3)
+    if pressedKey == '4': pointControll(-4)
+    if pressedKey == '5': pointControll(-5)
+    if pressedKey == '6': pointControll(-6)
+    if pressedKey == '0': pointControll(0)
+
+
+    if pressedKey == 'w': motorsControll('up')
+    if pressedKey == 's': motorsControll('down')
+    if pressedKey == ' ':
+        if (log == 0):
+            motorsControll('take')
+            log = 1
+        else:
+            motorsControll('drop')
+            log = 0
+    if pressedKey == 'q':
+        pointControll(0)
+        break
+
+
+
+motorsReset()
